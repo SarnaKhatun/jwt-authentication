3b4e4c25077f5a3a9da4a317ad7752111334d20f
Moloy das <classicitdevteam@gmail.com>
2025-03-13 15:06:37 +0600

    first commit

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    //Register API (POST, formdata)
+    public function register()
+    {
+
+    }
+
+    //Login API (POST, formdata)
+    public function login()
+    {
+
+    }
+
+    //Profile API (GET)
+    public function profile()
+    {
+
+    }
+
+    //Refresh Token API (GET)
+    public function refreshToken()
+    {
+
+    }
+
+    //Logout API (GET)
+    public function logout()
+    {
+
+    }
+
+}
